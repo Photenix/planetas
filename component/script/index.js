19e832dfff673a9d5c6fb526425bf58aa8e2f70e
Photenix <juanmanuelpinor@gmail.com>
2022-06-29 10:52:45 -0500

    Recorer array

---
+++ component/script/index.js
@@ -0,0 +1,15 @@
+let nombrePLanetas = Array('Tierra','Marte')
+let distancia = [6,3,'67']
+const tamaño = []
+
+for (let i = 0; i < nombrePLanetas.length; i++) {
+    const element = nombrePLanetas[i];
+    console.log(element)
+}
+
+let i = 0
+while (i <= nombrePLanetas.length - 1) {
+    console.log(i)
+    console.log(nombrePLanetas[i])
+    i++
+}
